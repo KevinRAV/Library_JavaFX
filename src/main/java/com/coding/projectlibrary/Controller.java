18b055aae28b2874b79937bf024cbd3e23f65d18
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2021-12-09 10:44:09 +0100

    opti notFixed

--- src/main/java/com/coding/projectlibrary/Controller.java
+++ src/main/java/com/coding/projectlibrary/Controller.java
@@ -17,6 +17,8 @@ import java.util.Calendar;
 import java.util.NoSuchElementException;
 import java.util.ResourceBundle;
 
+import static javafx.scene.input.MouseEvent.*;
+
 public class Controller implements Initializable {
 
     @FXML
@@ -81,99 +83,83 @@ public class Controller implements Initializable {
 
     public static ObservableList<Tab> tablist;
 
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        if(event.getSource() == btnAjout){
-            ajoutTab();
-        }else if(event.getSource() == btnRemove){
-            removeTab();
-        }else if(event.getSource() == btnUpdate){
-            updateTab();
-        }else if(event.getSource() == btnValidez){
-            try{
-                validezTab();
-            }catch (Exception e){
-                System.out.println("error validezBtn");
-            }
-        }
-    }
-
-    public void validezTab(){
-        Tab tabs = new Tab(txtTitre.getText(), txtAuteur.getText(), Integer.parseInt(txtParution.getText()), Integer.parseInt(txtColonne.getText()), Integer.parseInt(txtRange.getText()), txtResume.getText());
-        String getTitre = txtTitre.getText();
-        String getAuteur = txtAuteur.getText();
-        int getParution = Integer.parseInt(txtParution.getText());
-        int getColonne = Integer.parseInt(txtColonne.getText());
-        int getRange = Integer.parseInt(txtRange.getText());
-        String getResume = txtResume.getText();
-        int year = Calendar.getInstance().get(Calendar.YEAR);
-
-        if(getTitre.trim().isEmpty()){
-            System.out.println("error Titre");
-        }else if(getAuteur.trim().isEmpty()){
-            System.out.println("error Auteur");
-        } else if(getRange < 1 || getRange > 7 ) {
-            System.out.println("error Range");
-        }else if(getColonne < 1 || getColonne > 5){
-            System.out.println("error Colonne");
-        }else if(getParution > year){
-            System.out.println("error date");
-        }else if(getResume.trim().isEmpty()){
-            System.out.println("error Resume");
-        } else{
-            tablist = tabContainer.getItems();
-            tablist.add(tabs);
-            tabContainer.setItems(tablist);
-        }
-    }
-
-    public void ajoutTab() {
-        btnAjout.setOnMouseClicked(action->{
-            containerAll.getChildren().removeAll(containerGauche, containerDroite);
-            containerAll.getChildren().addAll(containerGauche, containerDroite);
-        });
-    }
-
-    public void removeTab() {
-
-//        Si aucune ligne n’est sélectionnée, l’utilisateur sera prévenu par un
-//        message d’erreur.
-
-        int getSelectedIndex = tabContainer.getSelectionModel().getSelectedIndex();
-        tabContainer.getItems().remove(getSelectedIndex);
-    }
-
-    public void updateTab(){
-        Tab tab = tabContainer.getSelectionModel().getSelectedItem();
-        tab.setTitre(txtTitre.getText());
-        tab.setAuteur(txtAuteur.getText());
-        tab.setParution(Integer.parseInt(txtParution.getText()));
-        tab.setColonne(Integer.parseInt(txtColonne.getText()));
-        tab.setRange(Integer.parseInt(txtRange.getText()));
-        tab.setResume(txtResume.getText());
-        tabContainer.refresh();
-    }
-
-    @FXML
-    private void handleMouseAction(MouseEvent event) {
-        Tab tab = tabContainer.getSelectionModel().getSelectedItem();
-        txtTitre.setText(tab.getTitre());
-        txtAuteur.setText(tab.getAuteur());
-        txtParution.setText(String.valueOf(tab.getParution()));
-        txtColonne.setText(String.valueOf(tab.getColonne()));
-        txtRange.setText(String.valueOf(tab.getRange()));
-        txtResume.setText(tab.getResume());
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+//        tabContainer.getSelectionModel().setCellSelectionEnabled(true);
+//        tabContainer.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
         containerAll.getChildren().removeAll(containerGauche, containerDroite);
+        Tab tab = tabContainer.getSelectionModel().getSelectedItem();
+
         colName.setCellValueFactory(new PropertyValueFactory<Tab, String>("titre"));
         colAuteur.setCellValueFactory(new PropertyValueFactory<Tab, String>("auteur"));
         colParution.setCellValueFactory(new PropertyValueFactory<Tab, Integer>("parution"));
         colColonne.setCellValueFactory(new PropertyValueFactory<Tab, Integer>("colonne"));
         colRange.setCellValueFactory(new PropertyValueFactory<Tab, Integer>("range"));
         colResume.setCellValueFactory(new PropertyValueFactory<Tab, String>("resume"));
+
+        btnValidez.setOnMouseClicked(MouseEvent -> {
+            Tab tabs = new Tab(txtTitre.getText(), txtAuteur.getText(), Integer.parseInt(txtParution.getText()), Integer.parseInt(txtColonne.getText()), Integer.parseInt(txtRange.getText()), txtResume.getText());
+            String getTitre = txtTitre.getText();
+            String getAuteur = txtAuteur.getText();
+            int getParution = Integer.parseInt(txtParution.getText());
+            int getColonne = Integer.parseInt(txtColonne.getText());
+            int getRange = Integer.parseInt(txtRange.getText());
+            String getResume = txtResume.getText();
+            int year = Calendar.getInstance().get(Calendar.YEAR);
+
+            if (getTitre.trim().isEmpty()) {
+                System.out.println("error Titre");
+            } else if (getAuteur.trim().isEmpty()) {
+                System.out.println("error Auteur");
+            } else if (getRange < 1 || getRange > 7) {
+                System.out.println("error Range");
+            } else if (getColonne < 1 || getColonne > 5) {
+                System.out.println("error Colonne");
+            } else if (getParution > year) {
+                System.out.println("error date");
+            } else if (getResume.trim().isEmpty()) {
+                System.out.println("error Resume");
+            } else {
+                tablist = tabContainer.getItems();
+                tablist.add(tabs);
+                tabContainer.setItems(tablist);
+            }
+        });
+
+        tabContainer.setOnMouseClicked(event->{
+            txtTitre.setText(tab.getTitre());
+            txtAuteur.setText(tab.getAuteur());
+            txtParution.setText(String.valueOf(tab.getParution()));
+            txtColonne.setText(String.valueOf(tab.getColonne()));
+            txtRange.setText(String.valueOf(tab.getRange()));
+            txtResume.setText(tab.getResume());
+        });
+
+        btnAjout.setOnAction(MouseEvent->{
+            btnAjout.setOnMouseClicked(action-> {
+                containerAll.getChildren().removeAll(containerGauche, containerDroite);
+                containerAll.getChildren().addAll(containerGauche, containerDroite);
+            });
+        });
+
+        btnRemove.setOnAction(MouseEvent->{
+            int getSelectedIndex = tabContainer.getSelectionModel().getSelectedIndex();
+            tabContainer.getItems().remove(getSelectedIndex);
+        });
+
+        btnUpdate.setOnAction(MouseEvent->{
+            tab.setTitre(txtTitre.getText());
+            tab.setAuteur(txtAuteur.getText());
+            tab.setParution(Integer.parseInt(txtParution.getText()));
+            tab.setColonne(Integer.parseInt(txtColonne.getText()));
+            tab.setRange(Integer.parseInt(txtRange.getText()));
+            tab.setResume(txtResume.getText());
+            tabContainer.refresh();
+        });
+
     }
 
 }
