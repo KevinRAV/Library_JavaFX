44731183d76d73696257e6bf28fdb7673d87195e
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2021-12-10 15:05:43 +0100

    commit --mainAll1

--- src/main/java/com/coding/projectlibrary/controller/SoldierController.java
+++ src/main/java/com/coding/projectlibrary/controller/SoldierController.java
@@ -1,9 +1,9 @@
 package com.coding.projectlibrary.controller;
 
-import com.coding.projectlibrary.model.armee;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
 import java.net.URL;
@@ -12,16 +12,18 @@ import java.util.ResourceBundle;
 public class SoldierController implements Initializable {
 
     @FXML
-    private TreeView<?> treeviewTest;
+    private TreeView<String> treeviewTest;
 
     @FXML
     private TextField valueG;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        treeviewTest.setOnMouseClicked(e->{
-            valueG.getText();
-
-        });
+            TreeItem<String> Armee = new TreeItem<>("Armée");
+            TreeItem<String> General = new TreeItem<>("General");
+            treeviewTest.setRoot(Armee);
+            TreeItem<String> Soldat = new TreeItem<>("Soldat");
+            Armee.getChildren().addAll(General);
+            General.getChildren().addAll(Soldat);
     }
 }
