0bb98b8009645b9002cb160d14955a957f4aa0af
HD4Code <h.djivandjy@gmail.com>
2021-12-10 10:00:07 +0100

    Mise à jour du IMC (photos)

--- src/main/java/com/coding/projectlibrary/controller/ImcController.java
+++ src/main/java/com/coding/projectlibrary/controller/ImcController.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
@@ -57,6 +58,12 @@ public class ImcController implements Initializable {
     @FXML
     private AnchorPane imc;
 
+    private Image normal = new Image("file:src/main/java/com/coding/projectlibrary/image/normal.png");
+    private Image souspoids = new Image("file:src/main/java/com/coding/projectlibrary/image/SousPoids.png");
+    private Image surpoids = new Image("file:src/main/java/com/coding/projectlibrary/image/Surpoids.png");
+    private Image obesite = new Image("file:src/main/java/com/coding/projectlibrary/image/Obesite.png");
+    private Image obesiteSevere = new Image("file:src/main/java/com/coding/projectlibrary/image/ObesiteSevere.png");
+
 
 
     @Override
@@ -85,20 +92,24 @@ public class ImcController implements Initializable {
 
             float valeurK = Float.parseFloat(valeurKg.getText());
             float valeurC = Float.parseFloat(valeurCm.getText());
-            float imc =(valeurK / (valeurC  * valeurC ))*10000;
+            float imc = Math.round((valeurK / (valeurC  * valeurC ))*10000);
 
 
             if (imc < 18.5){
-                comment.setText(String.valueOf("Vous êtes en situation de sous poids : "+imc));
-//                imageImc.setImage();
+                comment.setText(String.valueOf("Vous êtes en sous poids : "+imc));
+                imageImc.setImage(souspoids);
             }else if(imc>= 18.5  &&  imc <= 24.9 ){
-                comment.setText(String.valueOf("Vous êtes en situation normale : "+imc));
+                comment.setText(String.valueOf("Vous êtes de corpulance normale : "+imc));
+                imageImc.setImage(normal);
             }else if(imc >= 25  &&  imc <= 29.9 ){
-                comment.setText(String.valueOf("Vous êtes en situation surpoids : "+imc));
+                comment.setText(String.valueOf("Vous êtes en surpoids : "+imc));
+                imageImc.setImage(surpoids);
             }else if(imc>= 30  &&  imc <= 34.9 ){
                 comment.setText(String.valueOf("Vous êtes en situation d'obésité : "+imc));
+                imageImc.setImage(obesite);
             }else if(imc > 35 ){
                 comment.setText(String.valueOf("Vous êtes en situation d'obésité severe : "+imc));
+                imageImc.setImage(obesiteSevere);
             }
             resultatImc.setText(String.valueOf(imc));
 
